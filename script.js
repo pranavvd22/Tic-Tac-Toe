cb8f521bba398475a928adb81173346d43c98292
pranavvd22 <77336495+pranavvd22@users.noreply.github.com>
2024-04-19 13:58:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ const checkWin = ()=>{
             document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Won!!!"
             gameEnd=true
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
-            document.querySelector(".line").style.width = "20vw";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            // document.querySelector(".line").style.width = "20vw";
+            // document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
             gameover.play();
         }
     })
@@ -67,7 +67,7 @@ reset.addEventListener('click',()=>{
 
     turn = "X";
     gameEnd=false;
-    document.querySelector(".line").style.width = "0vw";
+    // document.querySelector(".line").style.width = "0vw";
     document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
 })
